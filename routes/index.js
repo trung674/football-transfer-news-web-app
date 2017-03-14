2ac8771aeb3647a7a445a7a1167b0f771593dee3
thanhtrung674 <thanhtrung674@gmail.com>
2017-03-14 15:20:32 +0000

    add search feature and display the result to page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var router = express.Router();
 require('dotenv').config()
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'EMT Football Tweets' });
-});
-
-module.exports = router;
-
 var Twit = require('twit')
-
 var T = new Twit({
   consumer_key:         process.env.TWIT_CONSUMER_KEY,
   consumer_secret:      process.env.TWIT_CONSUMER_SECRET,
@@ -18,3 +9,22 @@ var T = new Twit({
   access_token_secret:  process.env.TWIT_ACCES_SECRET,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'EMT Football Tweets' });
+});
+
+router.post('/', function(req, res, next) {
+  if (req.body.player) var player = req.body.player;
+  if (req.body.team) var team = req.body.team;
+  if (req.body.author) var author = req.body.author;
+  var query = player + ' AND ' + team;
+  T.get('search/tweets', { q: query, count: 5 }, function(err, data, response) {
+    console.log(response);
+    res.render('index', {query: query, tweets: data.statuses});
+  });
+
+});
+
+module.exports = router;

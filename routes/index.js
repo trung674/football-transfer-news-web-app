6f002c58f903f91bab91e740460a4b314c325dc7
omorhefere <ooimoloame1@sheffield.ac.uk>
2017-03-21 16:57:12 +0000

    choice of search

--- routes/index.js
+++ routes/index.js
@@ -31,10 +31,10 @@ connection.connect(function(err) {
 //     console.log(results)
 // });
 
-//connection.query('SELECT * FROM tweet', function (error, results, fields) {
-//  if (error) throw error;
-//  console.log(results)
-//});
+connection.query('SELECT * FROM tweet', function (error, results, fields) {
+  if (error) throw error;
+  console.log(results)
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -45,13 +45,15 @@ router.post('/', function(req, res, next) {
     // The Twitter Search API can only search tweets that are published in the past 7 days
     // source: https://dev.twitter.com/rest/public/search
     var query = '';
+    var team = '';
+
     if (req.body.player) {
-      var player = req.body.player;
+      player=req.body.player;
       query = query + player + " OR " + splitQuery(player);
     }// + " OR " + completeQuery(player,1)
 
     if (req.body.team) {
-      var team = req.body.team;
+      team = req.body.team;
       query = query + ' AND ' + team + " OR " + splitQuery(team);
     }// + " OR " + completeQuery(team,1)
 
@@ -61,6 +63,8 @@ router.post('/', function(req, res, next) {
     }
 
     if (query !== '') {
+
+      if (req.body.api){
       T.get('search/tweets', {
           q: query,
           count: 100
@@ -69,11 +73,12 @@ router.post('/', function(req, res, next) {
 
               var tweet_id = data.statuses[tweet].id_str
 
-              var check = connection.query('SELECT * FROM tweet WHERE tweet_id =' + tweet_id, function(error, results, fields) {
+              var check = connection.query('SELECT * FROM tweet WHERE tweet_id =' + mysql.escape(tweet_id), function(error, results, fields) {
                   if (error)
                       throw error;
                   }
               );
+              console.log(typeof check.results)
 
               if (check.results = null) {
                   var tweet_text = data.statuses[tweet].text
@@ -88,7 +93,7 @@ router.post('/', function(req, res, next) {
                       created_at: created_at
                   };
                   // there's no need to insert this into a variable
-                  connection.query('INSERT INTO tweet SET ?', post, function(error, results, fields) {
+                  connection.query('INSERT INTO tweet SET ?', mysql.escape(post), function(error, results, fields) {
                       if (error)
                           throw error;
                       }
@@ -110,7 +115,7 @@ router.post('/', function(req, res, next) {
                   team: team,
                   author: author
               };
-              connection.query('INSERT INTO query SET ?', message, function(error, results, fields) {
+              connection.query('INSERT INTO query SET ?',message, function(error, results, fields) {
                   if (error)
                       throw error;
                   }
@@ -122,10 +127,56 @@ router.post('/', function(req, res, next) {
               classifiedTweets: classifiedTweets
           });
       });
+  }
+  else{
+     if (team !==''){
+       var id = []
+         var check = connection.query('SELECT query_id FROM query WHERE player_name LIKE = ? AND team LIKE =?',[player,team], function(error, results, fields) {
+             if (error){
+                 throw error;
+               }
+                 else{
+
+                      id = JSON.parse(JSON.stringify(results));
+
+                      console.log(id)
+                      my_query = id[Object.keys(id).length - 1].query_id
+
+                 }
+
+             }
+
+
+         );
+
+     }
+     else{
+       var id = []
+
+         var past = connection.query('SELECT query_id FROM query WHERE player_name = ?',  req.body.player, function(error, results, fields) {
+             if (error){
+                 throw error;
+             }
+             else{
+
+                  id = JSON.parse(JSON.stringify(results));
+
+                  console.log(id)
+                  my_query = id[Object.keys(id).length - 1].query_id
+             }
+         }
+         );
+    
+
+     }
+
+
+
+  }
     } else {
       res.render('index', {message: 'Empty string'});
     }
-}); 
+});
 
 module.exports = router;
 

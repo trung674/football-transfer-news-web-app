dc094a72387fff1256125eaa19cd679e6a5dae0c
Mahesha Kulatunga <mkkulatunga1@sheffield.ac.uk>
2017-05-08 01:53:07 +0100

    Added buttons to navigate results

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -36,3 +36,28 @@ $(document).ready(function() {
 */
 
 });
+
+// When the user scrolls down, show toTop button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("toTop").style.display = "block";
+    } else {
+        document.getElementById("toTop").style.display = "none";
+    }
+}
+
+// When button is clicked, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; 
+}
+
+function scrollDown() {
+    window.scrollBy(0, $(window).height());
+}
+
+function scrollUp() {
+    window.scrollBy(0, -1 * $(window).height());
+}
